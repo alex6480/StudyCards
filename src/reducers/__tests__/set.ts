f8024d3f4a0bc5d96f12f7a01db6e8f5802546c5
Alexander Jordan <alexander@svorre.dk>
2018-10-23 12:58:47 -0400

    Add test for tags

--- src/reducers/__tests__/set.ts
+++ src/reducers/__tests__/set.ts
@@ -25,3 +25,35 @@ test("added card is added in the correct position", () => {
     expect(state.cardOrder).toStrictEqual(["cardId2", "cardId3", "cardId1", "cardId4"]);
     expect(state.filteredCardOrder.value).toStrictEqual(["cardId2", "cardId3", "cardId1", "cardId4"]);
 });
+
+describe("available tags", () => {
+    const setId = "setId123";
+    let state = fromSet.setValue({ id: setId }, fromActions.Action.addSetComplete(setId));
+    state = fromSet.setValue(state, fromActions.Action.addNewCardBegin("cardId1", setId));
+    state = fromSet.setValue(state, fromActions.Action.addNewCardBegin("cardId2", setId));
+    state = fromSet.setValue(state, fromActions.Action.addNewCardBegin("cardId3", setId));
+    expect(state.availableTags).toStrictEqual({});
+
+    state = fromSet.setValue(state, fromActions.Action.saveCardMetaBegin(setId, "cardId2", {
+        tags: ["tag1", "tag2"],
+    }));
+    expect(state.availableTags).toStrictEqual({
+        tag1: 1,
+        tag2: 1,
+    });
+
+    state = fromSet.setValue(state, fromActions.Action.saveCardMetaBegin(setId, "cardId3", {
+        tags: ["tag2", "tag3"],
+    }));
+    expect(state.availableTags).toStrictEqual({
+        tag1: 1,
+        tag2: 2,
+        tag3: 1,
+    });
+
+    state = fromSet.setValue(state, fromActions.Action.deleteCardBegin(setId, "cardId3"));
+    expect(state.availableTags).toStrictEqual({
+        tag1: 1,
+        tag2: 1,
+    });
+});
